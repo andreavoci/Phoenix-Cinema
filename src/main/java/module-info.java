eb81ff64e63d2ce8f882e78973f2054c78abe15b
andreavoci <andreavoci00@gmail.com>
2022-09-27 10:57:01 +0200

    Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.phoenix {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.phoenix to javafx.fxml;
+    exports com.example.phoenix;
+}
